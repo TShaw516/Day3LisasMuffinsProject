11e0cbfef3d32d76fd819824455fda47ddbdf83f
TShaw516 <TShaw516@gmail.com>
2020-01-15 13:13:21 -0500

    Lisas Muffins Practice Project: Day 3

---
+++ LisasMuffins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LisasMuffins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize variable
+            int cupsOfSugarRecipe = 7;
+            int currentCupsOfSugar = 2;
+            int remainingCupsRequired = 0;
+
+            //variable computation
+            remainingCupsRequired = cupsOfSugarRecipe - currentCupsOfSugar;
+
+            //variable output
+            Console.WriteLine("Remaining cups required is {0}", remainingCupsRequired);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
